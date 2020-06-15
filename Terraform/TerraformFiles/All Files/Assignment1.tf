43dd3c3bc949ccb0653cdd0c55cae0c08e632aeb
Ankit Yadav <ankitjnyadav@gmail.com>
2020-06-15 09:49:20 +0530

    End To End Automation using Terraform

--- Terraform/TerraformFiles/All Files/Assignment1.tf
+++ Terraform/TerraformFiles/All Files/Assignment1.tf
@@ -39,10 +39,31 @@ resource "aws_default_vpc" "main" {
     Name = "Default VPC"
   }
 }
-resource "aws_key_pair" "TfAssignment1Key" {
-  key_name   = "Terraform-Assignment1"
-  public_key = file("/home/ay/PycharmProjects/DevOps/aws_terraform_key.pub")
+
+resource "tls_private_key" "Tf_Private_Key" {
+  algorithm = "RSA"
 }
+
+resource "aws_key_pair" "Tf_Public_Key" {
+  key_name   = "Tf_Public_Key"
+  public_key = tls_private_key.Tf_Private_Key.public_key_openssh
+
+
+  depends_on = [
+    tls_private_key.Tf_Private_Key
+  ]
+}
+
+resource "local_file" "key-file" {
+  content  = tls_private_key.Tf_Private_Key.private_key_pem
+  filename = "Tf_Private_Key.pem"
+
+
+  depends_on = [
+    tls_private_key.Tf_Private_Key
+  ]
+}
+
 
 resource "aws_security_group" "allow_http" {
   depends_on = [  aws_default_vpc.main ]
@@ -51,10 +72,26 @@ resource "aws_security_group" "allow_http" {
   vpc_id      = aws_default_vpc.main.id
 
   ingress {
-    description = "HTTP from VPC"
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
-    protocol    = "http"
+    protocol    = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -65,7 +102,7 @@ resource "aws_security_group" "allow_http" {
 resource "aws_instance" "TerraformAMI_Assignment1"{
   ami = var.Default_AMI_id
   instance_type = var.Default_InstanceType
-  key_name = aws_key_pair.TfAssignment1Key.key_name
+  key_name = aws_key_pair.Tf_Public_Key.key_name
   security_groups = [aws_security_group.allow_http.name]
   tags = {
     Name = "TerraformAMI_Assignment1"
@@ -74,7 +111,8 @@ resource "aws_instance" "TerraformAMI_Assignment1"{
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("/home/ay/Downloads/keycloudclass1.pem")
+    private_key = tls_private_key.Tf_Private_Key.private_key_pem
+    //file("/home/ay/PycharmProjects/DevOps/Terraform-Assignment1.pem")
     host     = aws_instance.TerraformAMI_Assignment1.public_ip
   }
 
@@ -90,19 +128,19 @@ resource "aws_instance" "TerraformAMI_Assignment1"{
 
 resource "null_resource" "MyPublicIpRedirection" {
   provisioner "local-exec" {
-    command = "echo ${aws_instance.Day3_Instance.public_ip} > publicIP.txt"
+    command = "echo ${aws_instance.TerraformAMI_Assignment1.public_ip} > publicIP.txt"
   }
 }
 
 resource "null_resource" "OpenTheWebSite" {
   depends_on=[null_resource.OpenTheWebSite1]
   provisioner "local-exec" {
-    command = "firefox ${aws_instance.Day3_Instance.public_ip}"
+    command = "firefox ${aws_instance.TerraformAMI_Assignment1.public_ip}"
   }
 }
 
 resource "aws_ebs_volume" "Terra_EBS3" {
-  availability_zone = aws_instance.Day3_Instance.availability_zone
+  availability_zone = aws_instance.TerraformAMI_Assignment1.availability_zone
   size              = 1
   tags = {
     Name = "Terra_EBS_D3"
@@ -112,7 +150,7 @@ resource "aws_ebs_volume" "Terra_EBS3" {
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.Terra_EBS3.id
-  instance_id = aws_instance.Day3_Instance.id
+  instance_id = aws_instance.TerraformAMI_Assignment1.id
   force_detach = true
 }
 
@@ -121,8 +159,9 @@ resource "null_resource" "OpenTheWebSite1" {
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("/home/ay/Downloads/keycloudclass1.pem")
-    host     = aws_instance.Day3_Instance.public_ip
+    private_key = tls_private_key.Tf_Private_Key.private_key_pem
+    //file("/home/ay/PycharmProjects/DevOps/Terraform-Assignment1.pem")
+    host     = aws_instance.TerraformAMI_Assignment1.public_ip
   }
   provisioner "remote-exec" {
     inline = [
@@ -134,7 +173,60 @@ resource "null_resource" "OpenTheWebSite1" {
   }
 }
 
-output "myTest" {
-  value = [aws_key_pair.TfAssignment1Key,
-          aws_security_group.allow_http]
+resource "aws_s3_bucket" "Bucket_Provisioning" {
+  bucket = "ankitjnyadav-tf-github-bucket"
+  acl    = "public-read"
+
+  tags = {
+    Name        = "ankitjnyadav-tf-github-bucket"
+    Environment = "Dev"
+  }
 }
+
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.Bucket_Provisioning.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+    custom_origin_config {
+            http_port = 80
+            https_port = 80
+            origin_protocol_policy = "match-viewer"
+            origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+        }
+    }
+    enabled = true
+    default_cache_behavior {
+        allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = local.s3_origin_id
+        forwarded_values {
+            query_string = false
+            cookies {
+               forward = "none"
+            }
+        }
+        viewer_protocol_policy = "allow-all"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+    }
+
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+}
+
+
+//output "myTest" {
+//  value = [aws_security_group.allow_http]
+//}
--- Terraform/TerraformFiles/IaaC.tf
+++ Terraform/TerraformFiles/IaaC.tf
@@ -1,8 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  //  We should not share it and upload it. Hence using user profile.
-  //  access_key = "my-access-key"
-  //  secret_key = "my-secret-key"
   profile    =  "terraform"
 }
 
@@ -20,10 +17,31 @@ resource "aws_default_vpc" "main" {
     Name = "Default VPC"
   }
 }
-resource "aws_key_pair" "TfAssignment1Key" {
-  key_name   = "Terraform-Assignment1"
-  public_key = file("/home/ay/PycharmProjects/DevOps/aws_terraform_key.pub")
+
+resource "tls_private_key" "Tf_Private_Key" {
+  algorithm = "RSA"
 }
+
+resource "aws_key_pair" "Tf_Public_Key" {
+  key_name   = "Tf_Public_Key"
+  public_key = tls_private_key.Tf_Private_Key.public_key_openssh
+
+
+  depends_on = [
+    tls_private_key.Tf_Private_Key
+  ]
+}
+
+resource "local_file" "key-file" {
+  content  = tls_private_key.Tf_Private_Key.private_key_pem
+  filename = "Tf_Private_Key.pem"
+
+
+  depends_on = [
+    tls_private_key.Tf_Private_Key
+  ]
+}
+
 
 resource "aws_security_group" "allow_http" {
   depends_on = [  aws_default_vpc.main ]
@@ -32,10 +50,26 @@ resource "aws_security_group" "allow_http" {
   vpc_id      = aws_default_vpc.main.id
 
   ingress {
-    description = "HTTP from VPC"
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
-    protocol    = "http"
+    protocol    = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -46,7 +80,7 @@ resource "aws_security_group" "allow_http" {
 resource "aws_instance" "TerraformAMI_Assignment1"{
   ami = var.Default_AMI_id
   instance_type = var.Default_InstanceType
-  key_name = aws_key_pair.TfAssignment1Key.key_name
+  key_name = aws_key_pair.Tf_Public_Key.key_name
   security_groups = [aws_security_group.allow_http.name]
   tags = {
     Name = "TerraformAMI_Assignment1"
@@ -55,7 +89,7 @@ resource "aws_instance" "TerraformAMI_Assignment1"{
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("/home/ay/Downloads/keycloudclass1.pem")
+    private_key = tls_private_key.Tf_Private_Key.private_key_pem
     host     = aws_instance.TerraformAMI_Assignment1.public_ip
   }
 
@@ -71,19 +105,19 @@ resource "aws_instance" "TerraformAMI_Assignment1"{
 
 resource "null_resource" "MyPublicIpRedirection" {
   provisioner "local-exec" {
-    command = "echo ${aws_instance.Day3_Instance.public_ip} > publicIP.txt"
+    command = "echo ${aws_instance.TerraformAMI_Assignment1.public_ip} > publicIP.txt"
   }
 }
 
 resource "null_resource" "OpenTheWebSite" {
   depends_on=[null_resource.OpenTheWebSite1]
   provisioner "local-exec" {
-    command = "firefox ${aws_instance.Day3_Instance.public_ip}"
+    command = "firefox ${aws_instance.TerraformAMI_Assignment1.public_ip}"
   }
 }
 
 resource "aws_ebs_volume" "Terra_EBS3" {
-  availability_zone = aws_instance.Day3_Instance.availability_zone
+  availability_zone = aws_instance.TerraformAMI_Assignment1.availability_zone
   size              = 1
   tags = {
     Name = "Terra_EBS_D3"
@@ -93,7 +127,7 @@ resource "aws_ebs_volume" "Terra_EBS3" {
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.Terra_EBS3.id
-  instance_id = aws_instance.Day3_Instance.id
+  instance_id = aws_instance.TerraformAMI_Assignment1.id
   force_detach = true
 }
 
@@ -102,8 +136,8 @@ resource "null_resource" "OpenTheWebSite1" {
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("/home/ay/Downloads/keycloudclass1.pem")
-    host     = aws_instance.Day3_Instance.public_ip
+    private_key = tls_private_key.Tf_Private_Key.private_key_pem
+    host     = aws_instance.TerraformAMI_Assignment1.public_ip
   }
   provisioner "remote-exec" {
     inline = [
@@ -115,7 +149,55 @@ resource "null_resource" "OpenTheWebSite1" {
   }
 }
 
-output "myTest" {
-  value = [aws_key_pair.TfAssignment1Key,
-          aws_security_group.allow_http]
+resource "aws_s3_bucket" "Bucket_Provisioning" {
+  bucket = "ankitjnyadav-tf-github-bucket"
+  acl    = "public-read"
+
+  tags = {
+    Name        = "ankitjnyadav-tf-github-bucket"
+    Environment = "Dev"
+  }
+}
+
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.Bucket_Provisioning.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+    custom_origin_config {
+            http_port = 80
+            https_port = 80
+            origin_protocol_policy = "match-viewer"
+            origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+        }
+    }
+    enabled = true
+    default_cache_behavior {
+        allowed_methods = ["GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = local.s3_origin_id
+        forwarded_values {
+            query_string = false
+            cookies {
+               forward = "none"
+            }
+        }
+        viewer_protocol_policy = "allow-all"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+    }
+
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
 }
